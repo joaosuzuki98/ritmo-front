fbb12ae577466fc0b518c62d1aa1bd884e9b41c1
João Suzuki <joaosuzuki98@outlook.com>
2026-09-22 12:13:47 -0300

    style: update prettier formatting rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,7 @@
 module.exports = {
-  arrowParens: 'avoid',
-  singleQuote: true,
-  trailingComma: 'all',
-};
+    arrowParens: 'avoid',
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+}
